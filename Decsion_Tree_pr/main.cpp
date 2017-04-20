448991ab4a5f7b4584bbcf16581c057cead4a58e
latdmc <l.a.t.d.m.c@gmail.com>
2017-04-20 14:39:11 -0700

    Initial Commit

---
+++ Decsion_Tree_pr/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Decsion_Tree_pr
+//
+//  Created by 劉祐任 on 20/04/2017.
+//  Copyright © 2017 劉祐任. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
